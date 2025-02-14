19b9c82be2929bd8c9aa8b71e15665b32ffd7979
Tristan-Hopkins <127470526+Tristan-Hopkins@users.noreply.github.com>
2025-02-14 15:04:47 -0800

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
 });
 
 // Start server
-const PORT = 3005;
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
